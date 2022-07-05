00b93ffa02d17880755b1605f322cf6dcd3b0845
Vladimir <vladimirerl@gmail.com>
2022-07-05 11:36:14 +0500

    init

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,32 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        Hamster bite = new Hamster();
+        bite.name = "Байт";
+        bite.age = 2;
+        bite.color = "Рыжий";
+        bite.weight = 350;
+
+        Hamster ninja = new Hamster();
+        ninja.name = "Ниндзя";
+        ninja.age = 1;
+        ninja.weight = 250;
+        ninja.color = "Белый";
+
+        Hamster kuzya = new Hamster();
+        kuzya.name = "Кузя";
+        kuzya.age = 3;
+        kuzya.weight = 450;
+        kuzya.color = "Пятнистый";
+
+        Hamster paskal = new Hamster();
+        paskal.name = "Паскаль";
+        paskal.age = 2;
+        paskal.weight = 320;
+        paskal.color = "Чёрный";
+
+        System.out.println(bite.name);
+        System.out.println(ninja.name);
+        System.out.println(kuzya.name);
+        System.out.println(paskal.name);
+    }
+}
